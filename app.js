fa32b32ce5792b0ec0cd243c3c4309bdc52202ab
Akilah Powell <atpjun@gmail.com>
2022-10-27 19:05:56 -0400

    jss and styling added

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-/* Imports */
+
 
 /* Get DOM Elements */
+const submitBtn = document.getElementById('submit');
+const resetBtn = document.getElementById('reset');
+const promptSection = document.getElementById('prompt');
+const fortuneSection = document.getElementById('fortune');
+const answerP = document.getElementById('answer');
+const answers = ['Yes, definitely',
+'It is certain',
+'It is decidedly so',
+'Without a doubt',
+'You may rely on it',
+'As I see it, yes',
+'Most Likely',
+'Outlook good',
+'Signs point to yes',
+'Reply hazy, try again',
+'Ask again later',
+'Better not tell you now',
+'Cannot predict now',
+'Concentrate and ask again',
+'Dont count on it',
+'My reply is no',
+'My sources say no',
+'Outlook not so good',
+'Very doubtful'];
 
-/* State */
 
-/* Events */
+function toggleSections() {
+    promptSection.classList.toggle('hide');
+    fortuneSection.classList.toggle('hide');
+}
 
-/* Display Functions */
+submitBtn.addEventListener('click', () => {
+    toggleSections();
 
-// (don't forget to call any display functions you want to run on page load!)
+    const randInt = Math.floor(Math.random() * answers.length);
+    const randomChoice = answers[randInt];
+
+    answerP.textContent = randomChoice; 
+});
+
+resetBtn.addEventListener('click', toggleSections);
