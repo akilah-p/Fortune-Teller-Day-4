6d861f0aeb79e765ba82e2b47ea9f8b3c5f1477d
Akilah Powell <atpjun@gmail.com>
2022-10-27 20:20:30 -0400

    more styling and personlization

--- app.js
+++ app.js
@@ -7,24 +7,24 @@ const promptSection = document.getElementById('prompt');
 const fortuneSection = document.getElementById('fortune');
 const answerP = document.getElementById('answer');
 const answers = ['Yes, definitely',
-'It is certain',
-'It is decidedly so',
-'Without a doubt',
-'You may rely on it',
-'As I see it, yes',
-'Most Likely',
-'Outlook good',
-'Signs point to yes',
-'Reply hazy, try again',
-'Ask again later',
-'Better not tell you now',
-'Cannot predict now',
-'Concentrate and ask again',
-'Dont count on it',
-'My reply is no',
-'My sources say no',
-'Outlook not so good',
-'Very doubtful'];
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don`t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'];
 
 
 function toggleSections() {
